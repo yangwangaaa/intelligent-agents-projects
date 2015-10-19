a049f1dc730cabbe30178f1e58641849735136d8
VladarS <somers.vladimir@gmail.com>
2015-10-19 18:45:00 +0200

    state class added

--- deliberative/src/template/DeliberativeTemplate.java
+++ deliberative/src/template/DeliberativeTemplate.java
@@ -1,10 +1,12 @@
 package template;
 
 /* import table */
-import logist.simulation.Vehicle;
+import java.util.ArrayList;
+
 import logist.agent.Agent;
 import logist.behavior.DeliberativeBehavior;
 import logist.plan.Plan;
+import logist.simulation.Vehicle;
 import logist.task.Task;
 import logist.task.TaskDistribution;
 import logist.task.TaskSet;
@@ -18,34 +20,34 @@ import logist.topology.Topology.City;
 public class DeliberativeTemplate implements DeliberativeBehavior {
 
 	enum Algorithm { BFS, ASTAR }
-	
+
 	/* Environment */
 	Topology topology;
 	TaskDistribution td;
-	
+
 	/* the properties of the agent */
 	Agent agent;
 	int capacity;
 
 	/* the planning class */
 	Algorithm algorithm;
-	
+
 	@Override
 	public void setup(Topology topology, TaskDistribution td, Agent agent) {
 		this.topology = topology;
 		this.td = td;
 		this.agent = agent;
-		
+
 		// initialize the planner
 		int capacity = agent.vehicles().get(0).capacity();
 		String algorithmName = agent.readProperty("algorithm", String.class, "ASTAR");
-		
+
 		// Throws IllegalArgumentException if algorithm is unknown
 		algorithm = Algorithm.valueOf(algorithmName.toUpperCase());
-		
+
 		// ...
 	}
-	
+
 	@Override
 	public Plan plan(Vehicle vehicle, TaskSet tasks) {
 		Plan plan;
@@ -65,7 +67,7 @@ public class DeliberativeTemplate implements DeliberativeBehavior {
 		}		
 		return plan;
 	}
-	
+
 	private Plan naivePlan(Vehicle vehicle, TaskSet tasks) {
 		City current = vehicle.getCurrentCity();
 		Plan plan = new Plan(current);
@@ -91,11 +93,24 @@ public class DeliberativeTemplate implements DeliberativeBehavior {
 
 	@Override
 	public void planCancelled(TaskSet carriedTasks) {
-		
+
 		if (!carriedTasks.isEmpty()) {
 			// This cannot happen for this simple agent, but typically
 			// you will need to consider the carriedTasks when the next
 			// plan is computed.
 		}
 	}
+
+	private class State {
+		public City city;
+		public ArrayList<Task> carriedTasks;
+		public ArrayList<Task> deliveredTasks;
+		public State(City city, ArrayList<Task> carriedTasks, ArrayList<Task> deliveredTasks) {
+			this.city = city;
+			this.carriedTasks = carriedTasks;
+			this.deliveredTasks = deliveredTasks;
+		}
+		
+	}
+	
 }
